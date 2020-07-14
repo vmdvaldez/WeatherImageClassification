7821a4c3681e90ff5c39325ba9e8be0977bbb59b
Vincent <vmdvaldez@hotmail.com>
2020-07-13 23:21:32 -0400

    Fix metadata misssing values, add script to rename downloaded files

--- metadata_parser/rename_downloaded.py
+++ metadata_parser/rename_downloaded.py
@@ -24,8 +24,6 @@ class metadata_map():
 	def get_features(self, img_id):
 		return self.n_metadata[img_id]
 
-	def get_available_metadata(self):
-		return self.n_metadata
 
 	def __init__(self):
 
@@ -61,41 +59,51 @@ class metadata_map():
 			img_ids += imgs
 
 
+		xclasses = ["foggy", "rain", "snow"]
 
-		print("imgs_ids * 4 is equal to set(img_ids): "len(img_ids) / 4 == len(set(img_ids)))
-		img_ids = list(set(img_ids))
-		# print(type(img_ids[0]))
+		for clss in xclasses:
+			rename_img = glob.glob("./pinterest_images/{}/*".format(clss))
+			for i in rename_img:
+				x = int(random.uniform(0,1000000))
 
+				if str(x) in img_ids:
+					continue
+				path = splitext(i)[0].rsplit('/', 1)[0]
+				os.rename(i, path + '/{}'.format(x) + ".jpg")
+
+
+		# print(len(img_ids))
+		# print(len(set(img_ids)))
 		# print(class_idxs)
 
-		features = ["hum", "tempm", "dewptm", "vism", "pressurem", "windchillm", "wgustm"]
+		# features = ["hum", "tempm", "dewptm", "vism", "pressurem", "windchillm", "wgustm"]
 
-		### Load Metadata
-		with open('metadata.json') as f:
-			metadata = json.load(f)
+		# ### Load Metadata
+		# with open('metadata.json') as f:
+		# 	metadata = json.load(f)
 
-		self.n_metadata = {}
-		for data in metadata:
-			try:
-				idx = img_ids.index(data['id']) # check whether image has a metadata if not skip
-				clss = get_class(idx, class_idxs) # check which class image belongs to
-				# print(idx, clss)
-				feat_list = []
-				for key, val in data['weather'].items():
-					if key in features:
-						feat_list.append({key : val}) #put all relevant features in list
-		# 				# print(key, val)
-				if(data['id'] == "3414285633"):
-					print(feat_list)
-				self.n_metadata[data['id']] = (feat_list, clss) # <id : weatherfeat[]> mapping
-				# print(self.n_metadata)
-			except Exception as e:
-				x = 0		
+		# self.n_metadata = {}
+		# for data in metadata:
+		# 	try:	
+		# 		idx = img_ids.index(data['id']) # check whether image has a metadata if not skip
+		# 		clss = get_class(idx, class_idxs) # check which class image belongs to
+		# 		# print(idx, clss)
+		# 		feat_list = []
+		# 		for key, val in data['weather'].items():
+		# 			if key in features:
+		# 				feat_list.append({key : val}) #put all relevant features in list
+		# # 				# print(key, val)
+		# 		# if(data['id'] == "12381129"):
+		# 		# 	print(feat_list)
+		# 		self.n_metadata[data['id']] = (feat_list, clss) # <id : weatherfeat[]> mapping
+		# 		# print(self.n_metadata)
+		# 	except Exception as e:
 				# print("{} img id NOT FOUND".format(data['id'])) #lots of images not found because we are using subset of original dataset
+				# x = 0
 				# print(x)
 		
-		print(len(img_ids)) 
-		print(len(self.n_metadata))
+		# print(len(img_ids)) # total of 8408 images 
+		# print(len(self.n_metadata)) ## 6509 images with metadata
 		# for key,val in self.n_metadata.items():
 			# print(key, val)
 			# break
@@ -114,10 +122,8 @@ class metadata_map():
 
 if __name__ == "__main__":
 	m = metadata_map()
-	feat = m.get_features("3414285633")
-	avail_metadata = m.get_available_metadata()
-	print(len(avail_metadata))
-	print(feat)
+	# feat = m.get_features("12381129")
+	# print(feat)
 
 # weather_feats = ['hum', 'tempm', 'dewptm', 'vism', 'pressurem', 'windchillm', 'wgustm']
 # for data in metadata:
