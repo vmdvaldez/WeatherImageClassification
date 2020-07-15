cb65a1b141066d5edb366a5dadd658e35f9c61f9
Vincent <vmdvaldez@hotmail.com>
2020-07-14 20:48:04 -0400

    Recode parser, fixed class classification logic error

--- metadata_parser/meta_parser.py
+++ metadata_parser/meta_parser.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import isfile, join, splitext
 import glob
 import random
+import pandas as pd
 
 
 class metadata_map():
@@ -47,104 +48,86 @@ class metadata_map():
 
 	def __init__(self):
 
-		def get_class(index,class_idxs):
+		def get_class(im_id):
+			for clss, img_ids in self.class_imgs.items():
+				try:
+					x = img_ids.index(im_id)
+					return clss
+				except:
+					continue
 
-			if index < class_idxs[0]:
-				return"cloudy"
-			elif index >= class_idxs[0] and index < class_idxs[1]:
-				return"foggy"
-			elif index >= class_idxs[1] and index < class_idxs[2]:
-				return "rain"
-			elif index >= class_idxs[2] and index < class_idxs[3]:
-				return "snow"
-			elif index >= class_idxs[3]:
-				return "sunny"
-			else:
-				print("asserted")
-				assert 0
+				return None
+
+
 		self.datasets =[]
 		classes = ["cloudy", "foggy", "rain", "snow", "sunny"]
 		dataset = ["train", "val", "test"]
 
+		
 		for dset in dataset:
+			self.class_imgs = {}
+			num_img_ids = 0
 			# Get Image Names (id)
-			class_idxs = []
-			img_ids = []
 			for clss in classes:
+				img_ids = []
 				# IMAGE_PATH="./weather/{}".format(clss) #path to image class
 				IMAGE_PATH="./weather_dataset/{}/{}".format(dset, clss)
 				imgs =  [im.split('.')[0] for im in listdir(IMAGE_PATH) if isfile(join(IMAGE_PATH, im))] #get all images names
-				if class_idxs != []:
-					class_idxs.append(class_idxs[-1] + len(imgs))
-				else:
-					class_idxs.append(len(imgs))
 				img_ids += imgs
+				img_ids = list(set(img_ids))
+				num_img_ids += len(img_ids)
+				if clss not in self.class_imgs.keys():
+					self.class_imgs[clss] = img_ids
+				else:
+					self.class_imgs[clss] += img_ids
+				
 
-
-
-			print("imgs_ids * 4 is equal to set(img_ids): ", len(img_ids) / 4 == len(set(img_ids)))
-			img_ids = list(set(img_ids))
-			# print(type(img_ids[0]))
-
-			# print(class_idxs)
-
-			features = ["hum", "tempm", "dewptm", "vism", "pressurem", "windchillm", "wgustm"]
+			# features = ["hum", "tempm", "dewptm", "vism", "pressurem", "windchillm", "wgustm"]
+			features = ["hum", "tempm", "dewptm", "pressurem"]
 
 			### Load Metadata
+			metadata = 0
 			with open('metadata.json') as f:
 				metadata = json.load(f)
 
+
 			n_metadata = {}
 			for data in metadata:
-				try:
-					idx = img_ids.index(data['id']) # check whether image has a metadata if not skip
-					clss = get_class(idx, class_idxs) # check which class image belongs to
-					# print(idx, clss)
-					feat_list = []
-					for key, val in data['weather'].items():
-						if key in features:
-							if val == '' or val == 'N/A':
-								val = -9999 
-							feat_list.append({key : val}) #put all relevant features in list
-			# 				# print(key, val)
-					# if(data['id'] == "3414285633"):
-					# 	print(feat_list)
-					n_metadata[data['id']] = (feat_list, clss) # <id : weatherfeat[]> mapping
-					# print(self.n_metadata)
-				except Exception as e:
-					x = 0		
-					# print("{} img id NOT FOUND".format(data['id'])) #lots of images not found because we are using subset of original dataset
-					# print(x)
+				clss = get_class(data['id']) # check which class image belongs to
+				if clss is None:
+					continue
+
+				feat_list = []
+				for key, val in data['weather'].items():
+					if key in features:
+						if val == '' or val == 'N/A':
+							val = -9999 
+						feat_list.append({key : val}) #put all relevant features in list
+				# if(data['id'] == "3414285633"):
+				# 	print(feat_list)
+				n_metadata[data['id']] = (feat_list, clss) # <id : weatherfeat[]> mapping
+				# print(self.n_metadata)
 			
-			print("Number of Image Ids: ", len(img_ids)) 
+			print("Number of Image Ids: ", num_img_ids)
 			print("Number of Image Ids with metadata: ", len(n_metadata))
-			# for key,val in self.n_metadata.items():
-				# print(key, val)
-				# break
 			self.datasets.append(n_metadata)
-			# the 8408 - 6509 = 1899 (which is the number of images we got online)
-		print(len(self.datasets))
+		# print(len(self.datasets))
 
 
 
 
-# wgust, windhcill, vism (lots of missing or -999 values)
-# auto encoder on missing data instead of doing the regeneration?
+# # wgust, windhcill, vism (lots of missing or -999 values)
+# # auto encoder on missing data instead of doing the regeneration?
 
 
-##TODO need to do for data augmenting (change data augment python script to rename to ID.<>.jpg to easily split)
+# ##TODO need to do for data augmenting (change data augment python script to rename to ID.<>.jpg to easily split)
 
 if __name__ == "__main__":
 	m = metadata_map()
-	feat = m.get_features("3414285633")
-	avail_metadata = m.get_available_metadata()
-	print(len(avail_metadata))
-	print(feat)
+	# feat = m.get_features("3414285633")
+	# avail_metadata = m.get_available_metadata()
+	# print(len(avail_metadata))
+	# print(feat)
 
-# weather_feats = ['hum', 'tempm', 'dewptm', 'vism', 'pressurem', 'windchillm', 'wgustm']
-# for data in metadata:
-# 	print('id: ', data['id'])
-# 	for feats in weather_feats:
-# 		print(feats, data['weather'][feats])
 
 
